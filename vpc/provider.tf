2f7ffb9c8ecf926071c394b8ad39ca56d9fe12f1
zaviour7560 <166792457+zaviour7560@users.noreply.github.com>
2025-03-08 15:27:10 +0530

    Update provider.tf

--- vpc/provider.tf
+++ vpc/provider.tf
@@ -1,1 +1,12 @@
-a
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.rds.endpoint
+}
+
+output "alb_dns" {
+  value = aws_lb.app_alb.dns_name
+}
+
