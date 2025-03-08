35f99ccf25404552e905a001f92dc426dccf8352
zaviour7560 <166792457+zaviour7560@users.noreply.github.com>
2025-03-08 15:51:22 +0530

    Update variable.tf

--- vpc/variable.tf
+++ vpc/variable.tf
@@ -1,1 +1,12 @@
-a
+variable "vpc_cidr" {
+  type = string
+}
+variable "pub_sub_cidr" {
+  type = list(string)
+}
+variable "pvt_sub_cidr" {
+  type = list(string)
+}
+variable "availability_zone" {
+  type = list(string)
+}
